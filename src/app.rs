ef6738e9af312937a8aa638d7d3983e98358c754
yskszk63 <yskszk63@gmail.com>
2017-05-10 16:41:49 +0900

    use graphics::color

--- src/app.rs
+++ src/app.rs
@@ -2,6 +2,7 @@ use opengl_graphics::{GlGraphics};
 use piston::input::{RenderArgs};
 use board::Board;
 use graphics::Transformed;
+use graphics::color;
 
 pub struct AppSettings {
     pub cell_size: u32,
@@ -20,11 +21,11 @@ impl Default for AppSettings {
             cell_size: 40,
             cols: 8,
             rows: 8,
-            background_color: hex_color("008800"),
-            focused_background_color: hex_color("00ff00"),
-            separator_color: hex_color("000000"),
-            black_stone_color: hex_color("000000"),
-            white_stone_color: hex_color("ffffff")
+            background_color: color::hex("008800"),
+            focused_background_color: color::hex("00ff00"),
+            separator_color: color::hex("000000"),
+            black_stone_color: color::hex("000000"),
+            white_stone_color: color::hex("ffffff"),
         }
     }
 }
@@ -83,38 +84,4 @@ impl <'a> App<'a> {
         }
     }
 
-}
-
-
-fn hex_color(color_code: &str) -> [f32; 4] {
-    let mut rgb_chars = color_code.chars();
-    let r: f32 = (hex_to_dec( rgb_chars.nth(0).unwrap() ) * 16
-        + hex_to_dec( rgb_chars.nth(0).unwrap() ) ) as f32 / 255f32;
-    let g: f32 = (hex_to_dec( rgb_chars.nth(0).unwrap() ) * 16
-        + hex_to_dec( rgb_chars.nth(0).unwrap() ) ) as f32 / 255f32;
-    let b: f32 = (hex_to_dec( rgb_chars.nth(0).unwrap() ) * 16
-        + hex_to_dec( rgb_chars.nth(0).unwrap() ) ) as f32 / 255f32;
-    return [r, g, b, 1.0];
-}
-
-fn hex_to_dec(c: char) -> i32 {
-    return match c {
-        '0' => 0,
-        '1' => 1,
-        '2' => 2,
-        '3' => 3,
-        '4' => 4,
-        '5' => 5,
-        '6' => 6,
-        '7' => 7,
-        '8' => 8,
-        '9' => 9,
-        'a' => 10,
-        'b' => 11,
-        'c' => 12,
-        'd' => 13,
-        'e' => 14,
-        'f' => 15,
-        _ => 0,
-    };
 }
